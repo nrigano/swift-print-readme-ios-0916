949c43f35ba27a26e5dc06e1fa14782770a56de7
Nick Rigano <nrigano@Executor.local>
2016-09-16 10:50:51 -0400

    Complete

---
+++ Favorites.playground/Contents.swift
@@ -0,0 +1,13 @@
+var favoriteWord = "cleave"
+var favoriteColor = "green"
+var favoriteMusician = "Faith No More"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+let color = "blue"
+print("The sky is \(color).")
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
